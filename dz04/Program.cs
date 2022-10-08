ab8b02fb34526ed527bcc75cf99678286a271829
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-08 20:02:27 +0300

    add dz04+

--- dz04/Program.cs
+++ dz04/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите пятизначное число: ");
 int n = int.Parse(Console.ReadLine());
-int n1 = n/10000;
-int n2 = (n/1000)%10;
-int n4 = (n%100)/10;
-int n5 = n%10;
+int n1 = n / 10000;
+int n2 = (n / 1000) % 10;
+int n4 = (n % 100) / 10;
+int n5 = n % 10;
 
-if (n1==n5 && n2==n4)
+if (n1 == n5 && n2 == n4)
 Console.WriteLine("Число является палиндромом.");
 else
 Console.WriteLine("Число НЕ является палиндромом.");
