a8fcee7b480a71ccdd4060e433cdc8b413ad0409
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-08 19:36:39 +0300

    add dz03

---
+++ dz03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int T = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Вывод диапазона координат: ");
+if (T == 1)
+    Console.WriteLine("x > 0 ; y > 0");
+else if (T == 2)
+    Console.WriteLine("x < 0 ; y > 0");
+else if (T == 3)
+    Console.WriteLine("x < 0 ; y < 0");
+else
+    Console.WriteLine("x > 0 ; y < 0");
