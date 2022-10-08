a1bea2dd3f292cb72bd9328969f2608ba25dea67
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-08 18:52:25 +0300

    add dz01

---
+++ dz01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n%6 == 0) 
+    Console.WriteLine("День недели выходной");
+else if (n%7 == 0)
+       Console.WriteLine("День недели выходной");
+else 
+        Console.WriteLine("День недели будний");
